ecaf40e16ffd0cb7c67c8fddf0c880b0140dc433
Thibault <th.jullion@gmail.com>
2016-01-12 17:41:05 +0100

    Ajout de la page des votes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,6 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/vote', function () {
+    return view('vote');
+});
 
 /*
 |--------------------------------------------------------------------------
