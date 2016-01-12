59c5e5c263fee9b7fa81c7fcca8eadc1eb307b74
Thibault <th.jullion@gmail.com>
2016-01-12 17:44:22 +0100

    Ajout de la page des votes (erreur)

--- resources/views/vote.blade.php
+++ resources/views/vote.blade.php
@@ -11,49 +11,57 @@
 
 <body>
 <header>
-    Création d'un concours photos
+    <img src="img/campagnes/1/logo.png" alt="">
+    - Concours photos
 </header>
-<div class="creer_campagne">
+<div class="vote">
     <div class="row">
-        <div class="col-xs-5 champ">Nom du concours</div>
-        <div class="col-xs-6 col-xs-offset-1 valeur">
-            <input type="text" value="">
+        <div class="col-xs-2 icone">
+            <img src="img/campagnes/1/icone.png" alt="">
+        </div>
+        <div class="col-xs-3 photo">
+            <img src="img/campagnes/1/photos/1.jpg" alt="">
+        </div>
+        <div class="col-xs-6 col-xs-offset-1 criteres">
+            <div class="title">Critere 1</div>
+            <div class="note">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div class="title">Critere 2</div>
+            <div class="note">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div class="title">Critere 3</div>
+            <div class="note">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
         </div>
     </div>
     <div class="row">
-        <div class="col-xs-5 champ">Votre logo</div>
-        <div class="col-xs-6 col-xs-offset-1 valeur">
-            <input type="file" class="boutons" value="">
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-xs-5 champ">Date de début</div>
-        <div class="col-xs-6 col-xs-offset-1 valeur">
-            <input type="text" value="">
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-xs-5 champ">Date de fin</div>
-        <div class="col-xs-6 col-xs-offset-1 valeur">
-            <input type="text" value="">
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-xs-5 champ">Icone du concours</div>
-        <div class="col-xs-6 col-xs-offset-1 valeur">
-            <input type="file" class="boutons" value="">
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-xs-5 champ">Texte d'accueil</div>
-        <div class="col-xs-6 col-xs-offset-1 valeur">
-            <input type="text" value="">
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-xs-5 champ">Texte de felicitation</div>
-        <div class="col-xs-6 col-xs-offset-1 valeur">
-            <input type="text" value="">
+        <div class="col-xs-3 col-xs-offset-2 averange">
+            <img src="img/icones/fleche_ave.png" class="fleche" alt="">
+            <div class="white">
+                <img src="img/campagnes/1/coeur_on.png" class="heart" alt="">
+                3 / 5
+            </div>
         </div>
     </div>
 </div>
