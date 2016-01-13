02077cad7f49c3960c3461fe49fd936ad5fe2566
jordan <croisicjordan@gmail.com>
2016-01-13 19:49:35 +0100

    test get post

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,7 +21,7 @@ Route::get('/album', function () {
     return view('choix_photo_facebook');
 });
 
-Route::post('/test', function () {
+Route::get('/test', function () {
     return view('test');
 });
 
