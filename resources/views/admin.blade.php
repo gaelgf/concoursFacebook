bea4afcae96e10f3e853b27df081c48391d3ed6b
jordan <croisicjordan@gmail.com>
2016-02-01 15:15:32 +0100

    admin menu lateral

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -6,9 +6,9 @@
     <meta charset='utf-8'>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="menuback.css">
+    <link rel="stylesheet" href="css/menuback.css">
     <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-    <script src="MenuBack.js"></script>
+    <script src="js/MenuBack.js"></script>
 
     <title>SB Admin 2 - Bootstrap Admin Theme</title>
 
