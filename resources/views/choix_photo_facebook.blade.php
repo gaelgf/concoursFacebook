cf9e1e9f85bac26804c749edc673cf700ddc4d5d
Thibault <th.jullion@gmail.com>
2016-02-02 15:57:46 +0100

    (8) Ajout d'une page de test de connexion a FB : redirection

--- resources/views/choix_photo_facebook.blade.php
+++ resources/views/choix_photo_facebook.blade.php
@@ -5,8 +5,8 @@ session_start();
 require_once "fb_sdk/src/Facebook/autoload.php";
 
 $fb = new Facebook\Facebook([
-        'app_id' => '916120625137591',
-        'app_secret' => '07e327936f04164ae38405974b923578',
+        'app_id' => '1545660598783922',
+        'app_secret' => 'ca702576b5815fbccba65343c8003198',
         'default_graph_version' => 'v2.5',
 ]);
 
