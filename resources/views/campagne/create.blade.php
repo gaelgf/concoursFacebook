137d30aa99a083d55f62c7497c1259f797f8db51
jordan <croisicjordan@gmail.com>
2016-02-02 23:21:58 +0100

    modif page create

--- resources/views/campagne/create.blade.php
+++ resources/views/campagne/create.blade.php
@@ -4,7 +4,7 @@
 <html>
 <head>
     <title>Look! I'm CRUDding</title>
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
 <div class="container">
