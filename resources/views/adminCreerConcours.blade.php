84eaf3caac90abaf93d109b2633659bbdef6f968
jordan <croisicjordan@gmail.com>
2016-02-02 15:50:20 +0100

    Page admin

---
+++ resources/views/adminCreerConcours.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset='utf-8'>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/menuback.css">
+    <link rel="stylesheet" href="css/formcontact.css">
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+
+    <title>SB Admin 2 - Bootstrap Admin Theme</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+
+<div id='cssmenu'>
+    <ul>
+        <li><a href='admin'><span>Home</span></a></li>
+        <li class='active'><a href='#'><span>Créer Concours</span></a></li>
+        <li><a href='adminVisuConcours'><span>Visualiser Concours</span></a></li>
+        <li class='last'><a href='adminModifConcours'><span>Modifier Concours</span></a></li>
+    </ul>
+</div>
+<div id="contenu">
+
+
+    <form action="toto.php" method="post" id="formcreate">
+
+        <fieldset>
+            <legend> Création de concours </legend>
+
+            <label for="nom">Entrez le nom du concours :</label>
+            <input type="text" name="nom" value="nom" id="nom" />
+
+
+            <p>Choisissez une couleur de thème : </p>
+            <input type="radio" name="theme" value="rouge" id="rouge" checked="checked" /> Rouge
+            <input type="radio" name="theme" value="bleu" id="bleu" checked="checked" /> Bleu
+            <input type="radio" name="theme" value="vert" id="vert" checked="checked" /> Vert
+
+
+            <p>Choisissez un logo à afficher : </p>
+            <input type="file" name="logo" />
+
+        </fieldset>
+
+
+        <p>
+            <input type="submit" value="Envoyer" />
+        </p>
+
+    </form>
+
+
+</div>
+
+
+</body>
+
+</html>
---
+++ resources/views/adminModifConcours.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset='utf-8'>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/menuback.css">
+    <link rel="stylesheet" href="css/formcontact.css">
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+
+    <title>SB Admin 2 - Bootstrap Admin Theme</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+
+<div id='cssmenu'>
+    <ul>
+        <li><a href='admin'><span>Home</span></a></li>
+        <li><a href='adminCreerConcours'><span>Créer Concours</span></a></li>
+        <li><a href='adminVisuConcours'><span>Visualiser Concours</span></a></li>
+        <li class='active'><a href='#'><span>Modifier Concours</span></a></li>
+    </ul>
+</div>
+<div id="contenu">
+
+
+    <form action="toto.php" method="post" id="formcreate">
+    <form action="toto.php" method="post" id="formcreate">
+
+        <fieldset>
+            <legend> Modification de concours </legend>
+
+            <label for="nom">Choisissez le concours à modifier:</label>
+            <SELECT name="nom" size="1">
+                <OPTION>Concours 1
+                <OPTION>Concours 2
+                <OPTION>Concours 3
+                <OPTION>Concours de bob
+                <OPTION>Concours 5
+            </SELECT>
+
+        </fieldset>
+
+
+        <p>
+            <input type="submit" value="Envoyer" />
+        </p>
+
+    </form>
+
+
+
+        <h3>Affichage du concours </h3>
+
+
+
+</div>
+
+
+</body>
+
+</html>
