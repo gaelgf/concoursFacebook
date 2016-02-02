8ddf0c01222b923aeda71890da0254eb1d7d67a9
Thibault <th.jullion@gmail.com>
2016-02-02 17:19:02 +0100

    (8) Ajout d'une page de test de connexion a FB : redirection

--- resources/views/choix_telechargement_photo.blade.php
+++ resources/views/choix_telechargement_photo.blade.php
@@ -34,6 +34,31 @@ $helper = $fb->getRedirectLoginHelper();
         <div class="select_picture">
             <div class="container_select">
                 <select class="album_list">
+                    <?php
+                        /*
+                    $response = $fb->get('/me?fields=albums{can_upload,name,id}', $_SESSION['facebook_access_token']);
+
+                    $graphNode = $response->getGraphNode();
+                    $albums = $graphNode->getField("albums");
+                    $i=0;
+
+                    $liste ="";
+
+                    foreach ($albums as $album) {
+                        $photos = "";
+
+
+                        $i++;
+                        $title = $album->getField("name");
+                        $id_album = $album->getField("id");
+                        $can_publish = $album->getField("can_upload");
+
+                        if( $can_publish ){
+                            $liste .='<option value="'.$id_album.'">'.$title.'</option>';
+                        }
+                    }
+                    ?>
+                    <?php echo $liste; */?>
                 </select>
                 <img src="img/icones/arrow_select.png" class="arrow" alt="">
             </div>
