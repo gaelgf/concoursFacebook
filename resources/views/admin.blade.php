bf3b440ea7ac7809486d1be6a2ff624f3f2fabd5
jordan <croisicjordan@gmail.com>
2016-02-02 15:48:07 +0100

    Ajout de page admin

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -8,7 +8,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/menuback.css">
     <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-    <script src="js/MenuBack.js"></script>
 
     <title>SB Admin 2 - Bootstrap Admin Theme</title>
 
@@ -19,27 +18,22 @@
 
 <div id='cssmenu'>
     <ul>
-        <li><a href='#'><span>Home</span></a></li>
-        <li class='active has-sub'><a href='#'><span>Products</span></a>
-            <ul>
-                <li class='has-sub'><a href='#'><span>Product 1</span></a>
-                    <ul>
-                        <li><a href='#'><span>Sub Product</span></a></li>
-                        <li class='last'><a href='#'><span>Sub Product</span></a></li>
-                    </ul>
-                </li>
-                <li class='has-sub'><a href='#'><span>Product 2</span></a>
-                    <ul>
-                        <li><a href='#'><span>Sub Product</span></a></li>
-                        <li class='last'><a href='#'><span>Sub Product</span></a></li>
-                    </ul>
-                </li>
-            </ul>
-        </li>
-        <li><a href='#'><span>About</span></a></li>
-        <li class='last'><a href='#'><span>Contact</span></a></li>
+        <li class='active'><a href='#'><span>Home</span></a></li>
+        <li><a href='adminCreerConcours'><span>Créer Concours</span></a></li>
+        <li><a href='adminVisuConcours'><span>Visualiser Concours</span></a></li>
+        <li class='last'><a href='adminModifConcours'><span>Modifier Concours</span></a></li>
     </ul>
 </div>
+<div id="contenu">
+
+<p></p>
+<p>Acces au site : </p>
+    <p> Facebook Canvas : <a href="https://apps.facebook.com/picturechallenge"> Canvas </a></p>
+    <p> Appli Heroku : <a href="https://fierce-refuge-2356.herokuapp.com/"> APPLI </a></p>
+
+
+</div>
+
 
 </body>
 
