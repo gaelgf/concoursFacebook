40523e10ef96b30476f0e8f41c71d2be9c0788a9
Thibault <th.jullion@gmail.com>
2016-02-02 16:04:48 +0100

    (8) Ajout d'une page de test de connexion a FB : redirection

--- resources/views/choix_photo_facebook.blade.php
+++ resources/views/choix_photo_facebook.blade.php
@@ -1,3 +1,18 @@
+<?php
+
+session_start();
+
+require_once "fb_sdk/src/Facebook/autoload.php";
+
+$fb = new Facebook\Facebook([
+        'app_id' => '1545660598783922',
+        'app_secret' => 'ca702576b5815fbccba65343c8003198',
+        'default_graph_version' => 'v2.5',
+]);
+
+$helper = $fb->getRedirectLoginHelper();
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,6 +33,30 @@
 <div class="select_picture">
     <div class="container_select">
         <select class="album_list">
+            <?php
+            $response = $fb->get('/me?fields=albums{can_upload,name,id}', $_SESSION['facebook_access_token']);
+
+            $graphNode = $response->getGraphNode();
+            $albums = $graphNode->getField("albums");
+            $i=0;
+
+            $liste ="";
+
+            foreach ($albums as $album) {
+                $photos = "";
+
+
+                $i++;
+                $title = $album->getField("name");
+                $id_album = $album->getField("id");
+                $can_publish = $album->getField("can_upload");
+
+                if( $can_publish ){
+                    $liste .='<option value="'.$id_album.'">'.$title.'</option>';
+                }
+            }
+           ?>
+           <?php echo $liste; ?>
         </select>
         <img src="img/icones/arrow_select.png" class="arrow" alt="">
     </div>
