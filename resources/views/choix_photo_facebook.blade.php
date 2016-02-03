b0e705904192dddd34b60eb064141d4806308128
Thibault <th.jullion@gmail.com>
2016-02-03 16:15:29 +0100

    Affichage images albums

--- resources/views/choix_photo_facebook.blade.php
+++ resources/views/choix_photo_facebook.blade.php
@@ -1,6 +1,35 @@
-@extends("templates/layout.blade.php");
+<?php
 
-@section("content")
+session_start();
+
+require_once "fb_sdk/src/Facebook/autoload.php";
+
+$fb = new Facebook\Facebook([
+        'app_id' => '1545660598783922',
+        'app_secret' => 'ca702576b5815fbccba65343c8003198',
+        'default_graph_version' => 'v2.5',
+]);
+
+$helper = $fb->getRedirectLoginHelper();
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Titre</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/selection_album.js"></script>
+</head>
+
+<body>
+<header>
+    <img src="img/campagnes/1/logo.png" alt="">
+    - Concours photos
+</header>
 <div class="select_picture">
     <div class="container_select">
         <select class="album_list">
@@ -157,5 +186,5 @@
         </ul>
     </div>
 </div>
-
-@endsection
+</body>
+</html>
