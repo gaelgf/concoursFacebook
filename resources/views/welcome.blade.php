0e508980c13d5d413f2da8db56b22be703bd581e
Thibault <th.jullion@gmail.com>
2016-02-03 16:09:04 +0100

    Affichage images albums

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,39 +1,6 @@
-<?php
+@extends("templates/layout.blade.php");
 
-session_start();
-
-if( !isset($_SESSION['facebook_access_token'])){
-
-require_once "fb_sdk/src/Facebook/autoload.php";
-
-$fb = new Facebook\Facebook([
-        'app_id' => '1545660598783922',
-        'app_secret' => 'ca702576b5815fbccba65343c8003198',
-        'default_graph_version' => 'v2.5',
-]);
-
-$helper = $fb->getRedirectLoginHelper();
-$permissions = ['public_profile', 'email','user_photos','publish_actions']; // optional
-$loginUrl = $helper->getLoginUrl('https://fierce-refuge-2356.herokuapp.com/validation_connexion', $permissions);
-
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <title>Titre</title>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <script type="text/javascript" src="js/jquery.js"></script>
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="js/selection_album.js"></script>
-</head>
-
-<body>
-<header>
-    <img src="img/campagnes/1/logo.png" alt="">
-    - Concours photos
-</header>
+@section("content")
 <div class="select_picture">
     <div class="container_select">
         <div class="container_pictures">
@@ -59,11 +26,4 @@ $loginUrl = $helper->getLoginUrl('https://fierce-refuge-2356.herokuapp.com/valid
         <a  class="boutons bouton_connexion" href=" <?php echo $loginUrl; ?>">Je participe !</a>
     </div>
 </div>
-</body>
-</html>
-<?php
-}
-else{
-    header('https://fierce-refuge-2356.herokuapp.com/choix_type_photo');
-}
-?>
+@endsection
