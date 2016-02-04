0ef3885870b99b2da407f1a35c269e2bcd7b17b0
Ju <th.jullion@gmail.com>
2016-02-04 17:05:41 +0100

    créaton baseController

--- controllers/indexController.class.php
+++ controllers/indexController.class.php
@@ -1,21 +1,31 @@
 <?php
-class indexController{
-    public function indexAction( $args ){
 
-        $campagne = new campagne();
-        $campagne->setTitle("Campagne 1");
-        $campagne->setDescription("Lorem Ipsum");
+include "baseController.class.php" ;
 
-        $campagne->save();
+class indexController extends baseController{
+    public function indexAction( $args )
+    {
+
+        //$campagne = new campagne();
+        //$campagne->setTitle("Campagne 1");
+        //$campagne->setDescription("Lorem Ipsum");
+
+        //$campagne->save();
 
         $view = new view();
-        $view->setView("indexIndex");
 
-        $view->assign("valeur_1","32");
+
+        if (!isset($_SESSION['facebook_access_token'])) {
+
+            $helper = parent::conectionFacebook();
+            $permissions = ['public_profile', 'email','user_photos','publish_actions']; // optional
+            $loginUrl = $helper->getLoginUrl('https://fierce-refuge-2356.herokuapp.com/validation_connexion', $permissions);
+
+            $view->setView("indexIndex");
+
+            $view->assign("login_url", $loginUrl);
+        } else {
+            header("Location: ".parent::getPath()."choix");
+        }
     }
-
-    public function testAction( $args ){
-        echo "test2";
-    }
-
 }
--- view/indexIndex.php
+++ view/indexIndex.php
@@ -1,1 +1,28 @@
-<?php echo $valeur_1; ?>
+
+
+
+<div class="select_picture">
+    <div class="container_select">
+        <div class="container_pictures">
+            <ul class="album">
+                <li>
+                    <img src="assets/img/photo_exemple.jpg"></li>
+                </li>
+                <li>
+                    <img src="assets/img/photo_exemple.jpg"></li>
+                </li>
+                <li>
+                    <img src="assets/img/photo_exemple.jpg"></li>
+                </li>
+                <li>
+                    <img src="assets/img/photo_exemple.jpg"></li>
+                </li>
+                <li>
+                    <img src="assets/img/photo_exemple.jpg"></li>
+                </li>
+            </ul>
+        </div>
+        <br/>
+        <a  class="boutons bouton_connexion" href="<?php echo $login_url; ?>">Log in with Facebook!</a>
+    </div>
+</div>
