3fd705e78b946ee57ec5861e2212048f677c4e0e
gaelgf <gael.graffion@hotmail.fr>
2016-02-14 22:07:15 +0100

    test

--- controllers/participantController.class.php
+++ controllers/participantController.class.php
@@ -5,7 +5,9 @@ class participantController
 {
     public function saveAction($args)
     {
-        $participant = new participant( NULL, "27", "123456", "thibault", "jullion", "1990-04-15", true);
-        $participant->save();
+        //$participant = new participant( NULL, "27", "123456", "thibault", "jullion", "1990-04-15", true);
+        $thibault = participant::loadById(1);
+        var_dump($thibault);
+        echo $thibault->isParticipatingToCampagne(27);
     }
 }
