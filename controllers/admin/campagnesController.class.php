4619137384cc7c0b805b8f86a95c6a640700bef1
gaelgf <gael.graffion@hotmail.fr>
2016-02-22 13:13:05 +0100

    fix adminn

--- controllers/admin/campagnesController.class.php
+++ controllers/admin/campagnesController.class.php
@@ -11,16 +11,91 @@ class campagnesController{
             $errors = explode( ', ', $args['errors']);
         }
 
-        $currentCampagne = campagne::loadCurrent();
-        $inscrits = participant::loadParticipantsByCampagneId($currentCampagne->getId());
+        $campagne = campagne::loadCurrent();
+        $inscrits = participant::loadParticipantsByCampagneId($campagne->getId());
+        if ( !is_array( $campagne ) ) {
+            $inscrits = participant::loadParticipantsByCampagneId($campagne->getId());
+            $photos = photo::loadByCampagneId($campagne->getId());
+            
+            $participantsIds = [];         
+            foreach ($photos as $key => $photo) {
+                $participantsIds []= $photo->getIdParticipant();
+                $photosByParticipants[$key] = $photo;
+            }
+            $participants = [];
+            $participants = participant::loadByIds($participantsIds);
 
-        $view = new view();
-        $view->setView("admin/showCampagne", "adminlayout");
-        $view->assign("campagne", $currentCampagne);
-        $view->assign("inscrits", $inscrits);
-        if(isset($errors)) {
-            $view->assign("errorsMessages", $errors);
+            $photosIds = [];
+            $photosByParticipants = [];
+            foreach ($photos as $key => $photo) {
+                $photosIds []= $photo->getId();
+                $photosByParticipants[$key] = $photo;
+            }
+
+            $votes = vote::loadByPhotoIds($photosIds);
+            foreach ($votes as $key => $vote) {
+                $votesByPhotos[$vote->getIdPhoto()][] = $vote;
+            }
+
+            $test = [];
+            foreach($photos as $key => $photo) {
+                $finded = false; 
+                foreach ($votesByPhotos as $keyVote => $vote) {
+                    if($photo->getId() === $keyVote) {
+                        $test[$key] = $vote;
+                        $finded = true;
+                        break;
+                    }
+                }
+                if($finded === false) {
+                    $test[$key] = [];
+                }
+            }
+
+            $notes = [];
+            $criteres = critere::load();
+            foreach ($test as $key => $votesByPhoto) {
+                $notes[$key] = [];
+                foreach ($criteres as $critere) {
+                    $notes[$key][$critere->getNomCritere()] = 0;
+                }
+            }
+
+            foreach ($test as $key => $votesByPhoto) {
+                if(count($votesByPhoto) === 0) {
+                    foreach ($criteres as $critere) {
+                        $notes[$key][$critere->getNomCritere()] = 'aucun vote';
+                    }
+                } else {
+                    foreach ($votesByPhoto as $keyVote => $vote) {
+                        foreach ($criteres as $critere) {
+                            if($critere->getId() === $vote->getIdCritere()) {
+                                $notes[$key][$critere->getNomCritere()] += $vote->getValeur();
+                            }
+                        }
+                    }
+                    foreach ($criteres as $critere) {
+                        $notes[$key][$critere->getNomCritere()] /= count($votesByPhoto) / 3;
+                        $notes[$key][$critere->getNomCritere()] = $notes[$key][$critere->getNomCritere()];
+                    }
+                }                    
+            }
+
+            $view = new view();
+            $view->setView("admin/showCampagne", "adminlayout");
+            $view->assign("campagne", $campagne);
+            $view->assign("inscrits", $inscrits);
+            $view->assign("participants", $participants);
+            $view->assign("photosByParticipants", $photosByParticipants);
+            $view->assign("notesByParticipants", $notes);
+            if(isset($errors)) {
+                $view->assign("errorsMessages", $errors);
+            }
+            if(isset($success)) {
+                $view->assign("successMessages", $success);
+            }
         }
+
     }
 
     public function showallaction( $args )
