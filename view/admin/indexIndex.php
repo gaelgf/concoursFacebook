31171e2292b9cbd1e5ef8b2930e714db51797f62
gaelgf <gael.graffion@hotmail.fr>
2016-02-23 18:19:37 +0100

    fix

--- view/admin/indexIndex.php
+++ view/admin/indexIndex.php
@@ -1,7 +1,7 @@
 <h1 style="margin-top:70px;">Admin Concours Photos</h1>
 <div class="adminLoginContainer">
 	<?php if( isset($login_url)) { ?>
-	<a class="boutons bouton_connexion" href="<?php echo $login_url; ?>">Se connecter</a>
+	<a class="boutons bouton_connexion" target="_top" href="<?php echo $login_url; ?>">Se connecter</a>
 	<?php } ?>
 	<?php if( isset($notAdmin)) { 
 		echo '<h1>' . $notAdmin . '</h1>';
--- view/indexIndex.php
+++ view/indexIndex.php
@@ -6,7 +6,7 @@
             </div>
             <img class="photo_lot" src="<?php echo $array_campagne["image_lot"]; ?>" alt="">
             <br/>
-            <a  class="boutons bouton_connexion" href="<?php echo $login_url; ?>">Je participe !</a>
+            <a  class="boutons bouton_connexion" target="_top" href="<?php echo $login_url; ?>">Je participe !</a>
         </div>
         <div class="col-xs-3 container_pictures">
             <img class="photo" src="<?php echo $array_campagne["photo_accueil_one"]; ?>">
